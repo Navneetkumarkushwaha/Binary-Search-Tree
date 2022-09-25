4f3c1ad2fb6af9c657a75fb4f7e707639c088e4c
Er. Navneet kushwaha <76491523+Navneetkumarkushwaha@users.noreply.github.com>
2022-09-25 14:48:32 +0530

    Create 06 - Find LCA of two nodes in BST.py

---
+++ 06 - Find LCA of two nodes in BST.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if not root:
+            return None
+        
+        curr = root.val 
+        
+        if curr > p.val and curr > q.val:
+            
+            return self.lowestCommonAncestor(root.left,p,q)
+        
+        if curr < p.val and curr < q.val:
+            
+            return self.lowestCommonAncestor(root.right,p,q)
+        
+        return root
+        
+        
+        
+  TC  : - O(H)
+  SC : - O(1)
